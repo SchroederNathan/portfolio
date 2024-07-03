26b401e377740287004b824587313740335f5f1b
Nathan Schroeder <schroedernathan2011@icloud.com>
2024-07-02 22:49:49 -0400

    Adjusted landing page text style

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,21 @@ function App() {
 
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center bg-pattern">
+      <div className="min-h-screen flex items-center justify-center bg-custom-dark">
         <div className="text-left ">
-          <p className="text-blue-400">Hi, My Name is</p>
-          <h1 className="text-5xl mt-2 font-bold text-white">Nathan Schroeder</h1>
-          <p className="text-2xl text-blue-400 italic">Designer/Coder</p>
-          <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded">Download CV</button>
+          <p className="text-blue-400 font-source-code-pro">Hi, My Name is</p>
+          <h1 className="mt-5 mb-3 font-semibold text-mtext-white  text-8xl">Nathan<br/> Schroeder.</h1>
+          <p className="text-5xl mb-5 text-mtext-white italic font-source-code-pro">Designer/Coder.</p>
+          <button className="mt-4 w-[212px] h-[64px] text-2xl  font-semibold bg-accent text-custom-dark  rounded-lg">Download CV</button>
         </div>
-        <div className='absolute bottom-0 left-10 h-[300px] w-[100px]'>
-          <img src='images/discord.svg' className="h-[33px] absolute bottom-32 left-0 right-0 m-auto"></img>
-          <img src='images/instagram.svg' className="h-[33px] absolute bottom-44 left-0 right-0 m-auto"></img>
-          <img src='images/github.svg' className="h-[33px] absolute bottom-56 left-0 right-0 m-auto"></img>
-          <img src='images/linkedin.svg' className="h-[33px] absolute bottom-68 left-0 right-0 m-auto"></img>
 
-          <div className="h-[112px] bg-twhite w-[3px] absolute bottom-0 left-0 right-0 m-auto"></div>
+        <div className='absolute bottom-0 left-10 h-[300px] w-[100px]'>
+          <img src='images/discord.svg' className="h-[24px] absolute bottom-20 left-0 right-0 m-auto"></img>
+          <img src='images/instagram.svg' className="h-[24px] absolute bottom-32 left-0 right-0 m-auto"></img>
+          <img src='images/github.svg' className="h-[24px] absolute bottom-44 left-0 right-0 m-auto"></img>
+          <img src='images/linkedin.svg' className="h-[24px] absolute bottom-56 left-0 right-0 m-auto"></img>
+
+          <div className="h-[58px] bg-mtext-white w-[2px] absolute bottom-0 left-0 right-0 m-auto"></div>
         </div>
 
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,45 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ['"Montserrat"', ...defaultTheme.fontFamily.sans],
+      'source-code-pro': ['"Source Code Pro"', ...defaultTheme.fontFamily.sans],
+
+    },
+
     extend: {
       colors: {
-        'custom-dark': '#0a0a23',
-        'twhite': '#E1E8F0'
+        'custom-dark': '#0F172A',
+        'accent': '#74C6FF',
+        'mtext-white': '#E1E8F0'
+      },
+      cursor: {
+        default: 'url(/images/cursor.png), default',
+        pointer: 'url(/images/cursor.png), pointer',
       },
     },
   },
   plugins: [],
 }
+/* Nathan Schroeder. */
+
+// position: absolute;
+// width: 531px;
+// height: 168px;
+// left: 255px;
+// bottom: 518px;
+
+// font-family: 'Montserrat';
+// font-style: normal;
+// font-weight: 600;
+// font-size: 96px;
+// line-height: 84px;
+
+// color: rgba(225, 232, 240, 0.945098);
+
