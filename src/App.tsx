d762343365fdfea6617cb9f3ebe23ed6d8d10d51
“Nathan <schroedernathan2011@icloud.com>
2024-07-07 00:01:52 -0400

    added gradient

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,30 @@ function App() {
   return (
     <>
 
-      <div className='bg-[url("/images/leftdots.png")] fixed bg-no-repeat w-full'>
+      <div className='bg-[url("/images/leftdots.png")] fixed bg-no-repeat w-full'> 
+      <div className="relative isolate px-6 lg:px-8">
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 -top-74 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-72"
+        >
+          <div
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+            className="relative left-[calc(50%-10rem)] -top-72 aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[35deg] bg-gradient-to-tr from-[#74C6FF] to-[#4a449e49] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[102.1875rem]"
+          />
+        </div>
+
+      {/* <div aria-hidden="true" className="absolute left-1/3 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6">
+        <div
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 67.5% 30.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+          className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#74C6FF] to-[#9089fc] opacity-30"
+        />
+      </div>   */}
       <div className='bg-[url("/images/navdots.png")] bg-no-repeat fixed top-0 right-0 w-[33%] h-full'/>
 
         <div className="min-h-screen flex flex-row items-center ">
@@ -64,8 +87,21 @@ function App() {
           </div>
 
         </div>
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        >
+          <div
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#74C6FF] to-[#9189fc00] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          />
+        </div>
       </div>
-      
+      </div>  
+
     </>
   )
 }
