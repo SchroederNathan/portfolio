5b9289ad5572f3283ab1a044a7615eaa1c6eea43
Nathan Schroeder <schroedernathan2011@icloud.com>
2024-07-08 02:40:13 -0400

    Fixed flexbox

--- src/App.tsx
+++ src/App.tsx
@@ -30,53 +30,57 @@ function App() {
       <div className="dotted-background w-screen h-screen relative isolate">
         <div className="relative w-full h-full">
 
-          <div className="min-h-screen flex flex-row items-center">
-            <div className="ml-52 flex-1 relative">
-              <p className="text-accent/75 text-2xl italic font-source-code-pro bg-gradient-to-tr from-sky-300 via-blue-500 to-blue-900 bg-clip-text text-transparent">
-                Hi, My Name is
-              </p>
-              <h1 className="mt-3 mb-3 font-semibold text-mtext-white text-8xl">
-                Nathan
-                <br /> Schroeder.
-              </h1>
-              <p className="text-5xl mb-5 text-mtext-white italic font-source-code-pro">
-                <ReactTyped strings={["Designer/Coder."]} typeSpeed={100} />
-              </p>
-              <button className="mt-4 w-[212px] h-[64px] text-2xl font-semibold bg-accent text-custom-dark rounded-lg">
-                Download CV
-              </button>
+          <div className="min-h-screen flex flex-row justify-center items-center ml-28">
+            <div className="basis-2/5 flex flex-col items-center relative">
+              <div>
+                <p className="text-accent/75 text-2xl italic font-source-code-pro bg-gradient-to-tr from-sky-300 via-blue-500 to-blue-900 bg-clip-text text-transparent text-left">
+                  Hi, My Name is
+                </p>
+                <h1 className="mt-3 mb-3 font-semibold text-white text-8xl text-left">
+                  Nathan
+                  <br /> Schroeder.
+                </h1>
+                <p className="text-5xl mb-5 text-white italic font-source-code-pro text-left">
+                  <ReactTyped strings={["Designer/Coder."]} typeSpeed={100} />
+                </p>
+                <button className="mt-4 w-[212px] h-[64px] text-2xl font-semibold bg-accent text-custom-dark rounded-lg">
+                  Download CV
+                </button>
 
-              {/* Nav headers */}
-              <div className="flex flex-col gap-4 mt-12">
-                {lineActive("HOME")}
-                {line("ABOUT")}
-                {line("EXPERIENCE")}
-                {line("PROJECTS")}
+                {/* Nav headers */}
+                <div className="flex flex-col gap-4 mt-12 items-start">
+                  {lineActive("HOME")}
+                  {line("ABOUT")}
+                  {line("EXPERIENCE")}
+                  {line("PROJECTS")}
+                </div>
               </div>
             </div>
 
-            <div className="flex-1">
-              Hello
+            <div className="basis-3/5 flex justify-center items-center">
+              <p className="text-xl text-white">Hello</p>
             </div>
+          </div>
 
-            <div className="fixed bottom-0 left-0 h-full w-[100px]">
-              <p className="absolute left-5 right-0 m-auto text-4xl font-semibold mt-6 bg-gradient-to-tr from-sky-300 via-blue-500 to-blue-900 bg-clip-text text-transparent">
-                &lt;/&gt;
-              </p>
-              <a href="https://discord.gg/9w3r4u3" className="h-[24px] absolute bottom-20 left-0 right-0 m-auto">
-                <img src="images/discord.svg" className="m-auto" />
-              </a>
-              <a href="https://instagram.com/nathanschroederr" className="h-[24px] absolute bottom-32 left-0 right-0 m-auto">
-                <img src="images/instagram.svg" className="m-auto" />
-              </a>
-              <a href="https://github.com/schroedernathan" className="h-[24px] absolute bottom-44 left-0 right-0 m-auto">
-                <img src="images/github.svg" className="m-auto" />
-              </a>
-              <a href="https://www.linkedin.com/in/nathan-schroeder-a40aa2210/" className="h-[24px] absolute bottom-56 left-0 right-0 m-auto">
-                <img src="images/linkedin.svg" className="m-auto" />
-              </a>
-              <div className="h-[58px] bg-mtext-white w-[2px] absolute bottom-0 left-0 right-0 m-auto"></div>
-            </div>
+
+
+          <div className="fixed bottom-0 left-0 h-full w-[100px]">
+            <p className="absolute left-5 right-0 m-auto text-4xl font-semibold mt-6 bg-gradient-to-tr from-sky-300 via-blue-500 to-blue-900 bg-clip-text text-transparent">
+              &lt;/&gt;
+            </p>
+            <a href="https://discord.gg/9w3r4u3" className="h-[24px] absolute bottom-20 left-0 right-0 m-auto">
+              <img src="images/discord.svg" className="m-auto" />
+            </a>
+            <a href="https://instagram.com/nathanschroederr" className="h-[24px] absolute bottom-32 left-0 right-0 m-auto">
+              <img src="images/instagram.svg" className="m-auto" />
+            </a>
+            <a href="https://github.com/schroedernathan" className="h-[24px] absolute bottom-44 left-0 right-0 m-auto">
+              <img src="images/github.svg" className="m-auto" />
+            </a>
+            <a href="https://www.linkedin.com/in/nathan-schroeder-a40aa2210/" className="h-[24px] absolute bottom-56 left-0 right-0 m-auto">
+              <img src="images/linkedin.svg" className="m-auto" />
+            </a>
+            <div className="h-[58px] bg-mtext-white w-[2px] absolute bottom-0 left-0 right-0 m-auto"></div>
           </div>
 
           {/* Background images */}
