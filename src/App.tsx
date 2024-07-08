9d29e42edd71950c341bc8bae6a0116e7485579c
“Nathan <schroedernathan2011@icloud.com>
2024-07-08 13:41:00 -0400

    Put animations when hovering over the nav links

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,25 @@ import Background from "./components/Background";
 
 const lineActive = (title: string) => {
   return (
-    <div className="flex items-center">
-      <div className="w-[64px] h-[0px] border-solid border border-mtext-white/90"></div>
-      <p className="text-mtext-white/90 font-medium ml-4">{title}</p>
-    </div>
+    <a href="#">
+      <div className="flex items-center">
+        <div className="w-[64px] h-[0px] border-solid border border-mtext-white/90"></div>
+        <p className="text-mtext-white/90 font-medium ml-4">{title}</p>
+      </div>
+    </a>
   );
 };
 
 const line = (title: string) => {
   return (
-    <div className="flex items-center">
-      <div className="w-[32px] h-[0px] border-solid border border-mtext-white/40"></div>
-      <p className="text-mtext-white/40 font-medium ml-4">{title}</p>
-    </div>
+    <a href="#">
+      <div className="flex items-center transition-all duration-300 group">
+        <div className="w-[32px] h-[0px] border-solid border border-white/40 transition-all duration-300 group-hover:border-white/60 group-hover:w-[64px]"></div>
+        <p className="text-white/40 font-medium ml-4 transition-all duration-300 group-hover:text-white/60">
+          {title}
+        </p>
+      </div>
+    </a>
   );
 };
 
@@ -31,7 +37,7 @@ function App() {
     <>
       <div className="dotted-background w-screen h-screen relative isolate">
         <div className="relative w-full h-full overflow-scroll">
-          <div className="min-h-screen flex flex-row max-lg:flex-wrap  justify-center items-center ml-28 max-sm:ml-0 max-lg:pt-10">
+          <div className="min-h-screen flex flex-row max-lg:flex-wrap justify-center items-center ml-28 max-sm:ml-0 max-lg:pt-10">
             <div className=" flex flex-col items-center relative">
               <div>
                 <p className=" text-2xl max-sm:text-lg italic font-source-code-pro bg-gradient-to-tr from-sky-300 via-blue-500 to-blue-900 bg-clip-text text-transparent text-left">
@@ -58,7 +64,7 @@ function App() {
               </div>
             </div>
 
-            <div className="flex w-full h-[42rem] m-5 bg-accent justify-center">
+            <div className="flex w-full h-[42rem] m-5 bg-accent/30 justify-center">
               <p className="text-xl text-white">Hello</p>
               <p className="text-xl text-white">Hello</p>
             </div>
