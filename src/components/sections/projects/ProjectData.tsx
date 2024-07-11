a16ff83a846aef7fc213d73bba12a9f2f6277a43
Nathan Schroeder <schroedernathan2011@icloud.com>
2024-07-11 18:11:28 -0400

    Added data to project detail

--- src/components/sections/projects/ProjectData.tsx
+++ src/components/sections/projects/ProjectData.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const ProjectData = [
     {
-        image: "images/footcare2you.jpg",
+        images: "images/footcare2you.jpg",
         name: "Footcare2You",
         description: "A freelanced web app for a medical foot service provider. Featuring a contact form, Google Map, and detailed service info, it showcases my skills in modern web design and development using Figma and React.",        
         tags: ["tag1", "tag2"],
@@ -10,7 +10,8 @@ export const ProjectData = [
     },    
     {
         video: "videos/H2M-Demo.mp4",
-        image: "images/footcare2you.jpg",
+        coverImage: "images/ipad-landscape.jpg",
+        images: ["images/devices-h2m.png"],
         name: "Inventory Managment System",
         company: "H2M Staging",
         description: "I led a team of 3 to develop a web app to track inventory coming in and out of warehouses and track which homes they're in. We built the front end with Angular and an API with NodeJS, ExpressJS, Sequelize, and MySQL to manage warehouse and staged home inventory data.",
--- src/components/sections/projects/ProjectDetail.tsx
+++ src/components/sections/projects/ProjectDetail.tsx
@@ -2,41 +2,30 @@ import React, { useEffect, useState } from 'react'
 import VideoPlayer from './VideoPlayer'
 import { ProjectData } from './ProjectData'
 
-interface Props {
-    video?: string,
-    name?: string,
-    company?: string,
-    description?: string,
-    images?: [string],
-    tags?: [string],
-
-}
-
-const ProjectDetail = (id: number) => {
-    const [project, setProject] = useState(ProjectData[id]);
+const ProjectDetail = ({ id }: { id: number }) => {
+    const project = ProjectData[id];
 
     useEffect(() => {
-        setProject(ProjectData[id]);
-        console.log(project.name);
 
     }
     ), [id];
 
     return (
-        <>
-            {/* <p className='text-mtext-white mb-8 text-2xl font-medium'>{project.name ?? "Loading..."}</p>
-        <p className='text-mtext-white/60 font-semibold'>{project.company ?? "Loading..."}</p>
+        <div className='md:mb-12 lg:mb-18'>
+            <p className='text-mtext-white  text-2xl font-medium'>{project.name ?? "Loading..."}</p>
+            <p className='text-mtext-white/60 mb-6 font-semibold'>{project.company ?? "Loading..."}</p>
             {project.video ?
                 <VideoPlayer videoSrc={project.video} type="video/mp4" />
                 :
                 null
             }
+            <p className='text-mtext-white mt-6'>{project.description ?? "Loading..."}</p>
 
-            <p className='text-mtext-white mt-3'>{project.description ?? "Loading..."}</p> */}
+            <img className='w-full mt-6' src={project.images[0]}  />
 
 
 
-        </>
+        </div>
     )
 }
 
