5feb3f9a7092fbc67598e23e48e86cf1106a6cf7
Nathan Schroeder <schroedernathan2011@icloud.com>
2024-07-11 19:17:02 -0400

    Designed back button for project detail screen

--- src/components/sections/projects/ProjectDetail.tsx
+++ src/components/sections/projects/ProjectDetail.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import VideoPlayer from './VideoPlayer'
 import { ProjectData } from './ProjectData'
+import { IconArrowLeft } from '@tabler/icons-react';
 
 const ProjectDetail = ({ id }: { id: number }) => {
     const project = ProjectData[id];
@@ -12,8 +13,13 @@ const ProjectDetail = ({ id }: { id: number }) => {
 
     return (
         <div className='md:mb-12 lg:mb-18'>
+            <a className='mb-3 flex items-center hover:cursor-pointer group/back'>
+                <IconArrowLeft stroke={2} className='text-mtext-white transition-colors group-hover/back:text-mtext-white/75' />
+                <p className='text-mtext-white ml-1 transition-all duration-200 ease-in-out group-hover/back:ml-2 group-hover/back:text-mtext-white/75'>Back</p>
+            </a>
             <p className='text-mtext-white  text-2xl font-medium'>{project.name ?? "Loading..."}</p>
             <p className='text-mtext-white/60 mb-6 font-semibold'>{project.company ?? "Loading..."}</p>
+
             {project.video ?
                 <VideoPlayer videoSrc={project.video} type="video/mp4" />
                 :
@@ -21,7 +27,7 @@ const ProjectDetail = ({ id }: { id: number }) => {
             }
             <p className='text-mtext-white mt-6 whitespace-pre-wrap'>{project.description[1] ?? "Loading..."}</p>
 
-            <img className='w-full mt-6' src={project.images[1]}  />
+            <img className='w-full mt-6' src={project.images[1]} />
 
             {/* <p className='text-mtext-white mt-6 whitespace-pre-wrap'>{project.description[0] ?? "Loading..."}</p> */}
 
