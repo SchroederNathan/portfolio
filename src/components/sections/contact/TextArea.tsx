21376fb355ebb27c9a48c29cfe58bd5f2fb8aab8
Nathan Schroeder <schroedernathan2011@icloud.com>
2024-07-14 23:53:56 -0400

    Removed unused prop

--- src/components/sections/contact/TextArea.tsx
+++ src/components/sections/contact/TextArea.tsx
@@ -8,7 +8,7 @@ export interface InputProps
     extends React.InputHTMLAttributes<HTMLTextAreaElement> { }
 
 const TextInput = React.forwardRef<HTMLTextAreaElement, InputProps>(
-    ({ className, type, ...props }, ref) => {
+    ({ className, type, ...props }) => {
         const radius = 100; // change this to increase the rdaius of the hover effect
         const [visible, setVisible] = React.useState(false);
 
