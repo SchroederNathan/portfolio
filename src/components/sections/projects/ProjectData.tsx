4edfe41ae53d5d98948afb4675b6e809e8961091
Nathan Schroeder <schroedernathan2011@icloud.com>
2024-08-14 11:23:01 -0400

    Added AI Chatbot project

--- src/components/sections/projects/ProjectData.tsx
+++ src/components/sections/projects/ProjectData.tsx
@@ -1,27 +1,54 @@
 export const ProjectData = [
-    {
-        images: [ "images/footcare2you-display.png", "images/footcare2you.jpg",],
-        name: "Footcare2You",
-        description: ["A freelanced web app for a medical foot service provider. Featuring a contact form, Google Map, and detailed service info, showcasing my skills in modern web design and development using Figma and React.",
-            "Footcare2You is a web app built with React to market and sell medical foot care services. I developed prototypes for the client using Figma to ensure they were happy with the end product. \n\nThe app features a contact form, an Interactive Google Map using Google Maps API, and detailed information about the services provided. " + 
-            "I decided to use Mantine UI for the design for clean and efficient code and to make it accessible for any users whether on mobile or desktop."
-        ],
-        tags: ["React", "Typescript", "Mantine UI", "Figma", "Vercel"],
-        link: "https://www.footcare2you.ca",
-    },
-    {
-        video: "videos/H2M-Demo.mp4",
-        images: ["images/devices-h2m.png","images/ipad-landscape.jpg"],
-        name: "Inventory Managment System",
-        company: "H2M Staging",
-        description: [
-            "Leading a team of 3 we developed a web app to track inventory coming in and out of warehouses and track which homes they're in. We built the front end with Angular and an API with NodeJS, ExpressJS, Sequelize, and MySQL to manage warehouse and staged home inventory data.",
-            "Our goal for this project was to create an efficient inventory tracking web application for H2M Staging that allows monitoring of warehouse activities, facilitating efficient planning and resource allocation. \n\n" +
-            "Our approach involves implementing a backend system that seamlessly manages data, routing, and communication with the frontend. This simplified the development process of our server-side application using Angular, " +
-            "ensuring smooth operation and interaction with the user interface. \n\nThis web application offers real-time tracking of inventory movements, providing H2M Staging withaccurate data on warehouse stock and furniture distribution. " + 
-            "By streamlining these processes, it enhances operational efficiency and provides valuable insights for the company's planning needs."
-        ],
-        tags: ["Angular", "NodeJS", "Express", "MySQL", "Sequelize", "Figma", "Heroku",  "Netlify"],
-        link: "",
-    },
-]
+  {
+    images: ["images/ai-devices.png", "images/ai-devices.png"],
+    name: "AI Chatbot",
+    description: [
+      "A simple AI Chatbot powered by GPT-3.5 Turbo. Featuring a modern design with light and dark mode toggles. Built with NextJS, TypeScript, and Shadcn/ui.",
+      "A simple AI Chatbot powered by GPT-3.5 Turbo. Featuring a modern design with light and dark mode toggles. Built with NextJS, TypeScript, and Shadcn/ui.",
+    ],
+    tags: [
+      "React",
+      "NextJS",
+      "Typescript",
+      "Shadcn/ui",
+      "GPT-3.5 Turbo",
+      "Vercel",
+    ],
+    link: ["https://ai-chatbot-five-sandy.vercel.app/", "https://github.com/SchroederNathan/AI-Chatbot"]
+  },
+  {
+    images: ["images/footcare2you-display.png", "images/footcare2you.jpg"],
+    name: "Footcare2You",
+    description: [
+      "A freelanced web app for a medical foot service provider. Featuring a contact form, Google Map, and detailed service info, showcasing my skills in modern web design and development using Figma and React.",
+      "Footcare2You is a web app built with React to market and sell medical foot care services. I developed prototypes for the client using Figma to ensure they were happy with the end product. \n\nThe app features a contact form, an Interactive Google Map using Google Maps API, and detailed information about the services provided. " +
+        "I decided to use Mantine UI for the design for clean and efficient code and to make it accessible for any users whether on mobile or desktop.",
+    ],
+    tags: ["React", "Typescript", "Mantine UI", "Figma", "Vercel"],
+    link: ["https://www.footcare2you.ca",]
+  },
+  {
+    video: "videos/H2M-Demo.mp4",
+    images: ["images/devices-h2m.png", "images/ipad-landscape.jpg"],
+    name: "Inventory Managment System",
+    company: "H2M Staging",
+    description: [
+      "Leading a team of 3 we developed a web app to track inventory coming in and out of warehouses and track which homes they're in. We built the front end with Angular and an API with NodeJS, ExpressJS, Sequelize, and MySQL to manage warehouse and staged home inventory data.",
+      "Our goal for this project was to create an efficient inventory tracking web application for H2M Staging that allows monitoring of warehouse activities, facilitating efficient planning and resource allocation. \n\n" +
+        "Our approach involves implementing a backend system that seamlessly manages data, routing, and communication with the frontend. This simplified the development process of our server-side application using Angular, " +
+        "ensuring smooth operation and interaction with the user interface. \n\nThis web application offers real-time tracking of inventory movements, providing H2M Staging withaccurate data on warehouse stock and furniture distribution. " +
+        "By streamlining these processes, it enhances operational efficiency and provides valuable insights for the company's planning needs.",
+    ],
+    tags: [
+      "Angular",
+      "NodeJS",
+      "Express",
+      "MySQL",
+      "Sequelize",
+      "Figma",
+      "Heroku",
+      "Netlify",
+    ],
+    link: ["",]
+  },
+];
--- src/components/sections/projects/ProjectDetail.tsx
+++ src/components/sections/projects/ProjectDetail.tsx
@@ -1,48 +1,68 @@
-import VideoPlayer from './VideoPlayer'
-import { ProjectData } from './ProjectData'
-import { IconArrowLeft } from '@tabler/icons-react';
-import TechChip from './TechChip';
-import { link } from '../../../Helpers';
+import VideoPlayer from "./VideoPlayer";
+import { ProjectData } from "./ProjectData";
+import { IconArrowLeft } from "@tabler/icons-react";
+import TechChip from "./TechChip";
+import { link } from "../../../Helpers";
 
-const ProjectDetail = ({ id, onBackClicked }: { id: number, onBackClicked: any }) => {
-    const project = ProjectData[id];
+const ProjectDetail = ({
+  id,
+  onBackClicked,
+}: {
+  id: number;
+  onBackClicked: any;
+}) => {
+  const project = ProjectData[id];
 
-    return (
-        <div className='md:mb-12 lg:mb-18' id='detail'>
-            <a className='mb-3 flex items-center hover:cursor-pointer group/back' onClick={() => onBackClicked()}>
-                <IconArrowLeft stroke={2} className='text-mtext-white transition-colors group-hover/back:text-mtext-white/75' />
-                <p className='text-mtext-white ml-1 transition-all duration-200 ease-in-out group-hover/back:ml-2 group-hover/back:text-mtext-white/75'>Back</p>
-            </a>
-            <p className='text-mtext-white text-2xl font-medium'>{project.name ?? "Loading..."}</p>
-            {project.company ?
-                <a href={project.link}>
-                    <p className='text-mtext-white/60 mb-6 font-semibold'>{project.company ?? "Loading..."}</p>
-                </a>
-                :
-                null
-            }
+  return (
+    <div className="md:mb-12 lg:mb-18" id="detail">
+      <a
+        className="mb-3 flex items-center hover:cursor-pointer group/back"
+        onClick={() => onBackClicked()}
+      >
+        <IconArrowLeft
+          stroke={2}
+          className="text-mtext-white transition-colors group-hover/back:text-mtext-white/75"
+        />
+        <p className="text-mtext-white ml-1 transition-all duration-200 ease-in-out group-hover/back:ml-2 group-hover/back:text-mtext-white/75">
+          Back
+        </p>
+      </a>
+      <p className="text-mtext-white text-2xl font-medium">
+        {project.name ?? "Loading..."}
+      </p>
+      {project.company ? (
+        <a href={project.link[0]}>
+          <p className="text-mtext-white/60 mb-6 font-semibold">
+            {project.company ?? "Loading..."}
+          </p>
+        </a>
+      ) : null}
 
-            {project.video ?
-                <VideoPlayer videoSrc={project.video} type="video/mp4" />
-                :
-                null
-            }
-            <p className='text-mtext-white mt-6 whitespace-pre-wrap'>{project.description[1] ?? "Loading..."}</p>
+      {project.video ? (
+        <VideoPlayer videoSrc={project.video} type="video/mp4" />
+      ) : null}
+      <p className="text-mtext-white mt-6 whitespace-pre-wrap">
+        {project.description[1] ?? "Loading..."}
+      </p>
 
-            {project.images[1] ?
-                <img className='w-full mt-6' src={project.images[0]} />
-                :
-                null
-            }
+      {project.images[1] ? (
+        <a href={project.link[0]}>
+          <img className="w-full mt-6" src={project.images[0]} />
+        </a>
+      ) : null}
 
-            {project.link ? <p className='text-mtext-white'><br />{link(project.link, "Visit Website")}</p> : null}
+      {project.link[0] ? (
+        <p className="text-mtext-white">
+          <br />
+          {link(project.link[0], "Visit Website")}
+        </p>
+      ) : null}
 
-            <ul className='mt-6 mb-6 flex flex-wrap'>
-                <TechChip tags={project.tags} />
-            </ul>
+      <ul className="mt-6 mb-6 flex flex-wrap">
+        <TechChip tags={project.tags} />
+      </ul>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default ProjectDetail
+export default ProjectDetail;
